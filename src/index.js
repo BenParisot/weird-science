2922e99825cdfa4ba1c1ac004185571290c93b01
Ben Parisot <me@benparisot.com>
2019-03-08 17:58:47 -0800

    clean up comments and console.logs

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import { queryToObject } from './hash-query-component.js';
 import './search-component.js';
 import { createApiURL } from './make-api-url-component.js';
 
-
-
-
 window.addEventListener('hashchange', () => {
     const existingQuery = window.location.hash.slice(1);
     const queryOptions = queryToObject(existingQuery);
     const corsURL = 'https://cors-anywhere.herokuapp.com/';
     const url = createApiURL(queryOptions);
     const fullURL = corsURL + url;
-    console.log(fullURL);
     fetch(fullURL)
         .then(response => response.json())
         .then(body => {
